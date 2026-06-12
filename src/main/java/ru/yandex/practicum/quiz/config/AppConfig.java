628418054d47ff4f6d4b11a0a95fd575afc8a6b0
Maxim Sergeev <sergeevsci@yandex.ru>
2026-06-12 23:04:06 +0300

    конфигурируем все. вынесли по файлам и папкам

--- src/main/java/ru/yandex/practicum/quiz/config/AppConfig.java
+++ src/main/java/ru/yandex/practicum/quiz/config/AppConfig.java
@@ -8,5 +8,28 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Setter @Getter @ToString
 @ConfigurationProperties("spring-quiz")
 public class AppConfig {
+
     private String title;
+    private ReportSettings report;
+
+    @Setter @Getter @ToString
+    public static class ReportSettings {
+        private boolean enabled;
+        private ReportMode mode;
+        private ReportOutputSettings output;
+    }
+
+    @Setter @Getter @ToString
+    public static class ReportOutputSettings {
+        private ReportOutputMode mode;
+        private String path;
+    }
+
+    public enum ReportMode {
+        VERBOSE, CONCISE;
+    }
+
+    public enum ReportOutputMode {
+        CONSOLE, FILE;
+    }
 }
